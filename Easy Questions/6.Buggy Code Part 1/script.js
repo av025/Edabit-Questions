c2e06655dba90cd8da53feb46dae3dce4a9af20c
Aayush Vyas <aayush25vyas@gmail.com>
2025-05-29 11:20:16 +0530

    Buggy Code Part 1

---
+++ Easy Questions/6.Buggy Code Part 1/script.js
@@ -0,0 +1,21 @@
+//! Buggy Code Part 1 
+//* Fix the code given below 
+
+// function cubes(a) {
+// 	retunr a ** 3
+// }
+
+//* above function cubes do operation and execute like this example : 
+// cubes(3)  ----> 27 
+// cubes(5)  ----> 125 
+// cubes(10) ----> 1000
+
+function cubes(a) {
+    return a ** 3 ; 
+    //* Here we use this asterik operator to find expoential value 
+}; 
+
+
+console.log(cubes(3));  // Output : 27 
+console.log(cubes(5));  // Output : 125 
+console.log(cubes(10)); // Output : 1000 
