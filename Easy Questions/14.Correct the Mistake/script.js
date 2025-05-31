66c5b88c77e6a54b6f9ce72b1ab3b23a9b222724
Aayush Vyas <aayush25vyas@gmail.com>
2025-05-31 14:42:35 +0530

    Correct the Mistake

---
+++ Easy Questions/14.Correct the Mistake/script.js
@@ -0,0 +1,21 @@
+//! Correct the Mistake 
+//* Fix the code to pass this challenge (only syntax errors). Look at the examples below to get an idea of what the function should do. 
+
+//?? Examples
+// squared(5) ----> 25
+// squared(9) ----> 81
+// squared(100) ----> 10000 
+
+//? Code to Fix
+// function squaed(b) {
+// 	return a * a
+// } 
+
+function squared(a) {
+    return a * a; 
+}; 
+
+
+console.log(squared(5));  // Output : 25 
+console.log(squared(9));  // Output : 81 
+console.log(squared(100)); // Output : 10000 
